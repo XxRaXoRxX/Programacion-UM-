c93f2ba3a04a60197c289367fd13bd52be8dea84
Markitos <raxor708@hotmail.com>
2022-04-08 13:30:09 -0300

    Intento de arreglo del dotenv no encontrando variable PORT

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -1,25 +1,32 @@
 import os
+import sys
 import resource
 from flask import Flask
-from dotenv import load_dotenv
+from dotenv import find_dotenv, load_dotenv
 from flask_restful import Api
 import main.resources as resource
 
 api = Api()
 
 def create_app():
+    
     #inicializar Flask
-    app= Flask(__name__)
+    app = Flask(__name__)
+    
     #Cargar variables de entorno
-    load_dotenv()
+    path = sys.path[0]
+    load_dotenv(path) 		#TODO: Por alguna razón esto no esta funcionando y no me trae el PORT dentro del .env. Consultar con profe
     api.add_resource(resource.PoemsResource, '/poems')
     api.add_resource(resource.PoemResource, '/poem/<id>')
     api.add_resource(resource.UsersResource, '/users')
     api.add_resource(resource.UserResource, '/user/<id>')
     api.add_resource(resource.MarksResource, '/marks')
     api.add_resource(resource.MarkResource, '/mark/<id>')
-    #Aqui se inicializaran el resto de los m
-    #retornar aplicaciion inicializada
+    
+    #Iniciar el resto de las extensiones.
+    #Extensiones
     api.init_app(app)
+    
+    #Retornar aplicación inicializada
     return app
 
