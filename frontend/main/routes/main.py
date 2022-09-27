ec003764f2bfec03e5e042589427a369e221d77c
Markitos <raxor708@hotmail.com>
2022-09-27 20:22:52 -0300

    Agregado funcionamiento del token en el login

--- frontend/main/routes/main.py
+++ frontend/main/routes/main.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, redirect, url_for, render_template
+from flask import Blueprint, redirect, url_for, render_template, make_response
+import requests
+import json
 
 #Crear Blueprint
 main = Blueprint('main', __name__, url_prefix='/')
@@ -11,4 +13,21 @@ def index():
 # Ruta menu principal con poemas
 @main.route('/login')
 def login():
-    return render_template('login.html')
+    # TODO: URL hardcodeada. Mandar esto a __init__.py
+    api_url = "http://127.0.0.1:8500/auth/login"
+
+    # Envio de logueo.
+    data = {"email": "usuario@usuario.com", "password": "123"}
+    headers = {"Content-Type" : "application/json"}
+    response = requests.post(api_url, json = data, headers = headers)
+
+    # Obtener el token desde response.
+    token = json.loads(response.text)
+    token = token["access_token"]
+
+    # Guardar el token en las cookies y devuelve la página.
+    resp = make_response(render_template("login.html"))
+    resp.set_cookie("access_token", token)
+    return resp
+
+    #return render_template('login.html')
